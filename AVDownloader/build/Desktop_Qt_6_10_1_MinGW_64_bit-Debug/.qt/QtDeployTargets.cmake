cff83d0b38dc2197a0ddf8058fa3d6436e226f55
WeN1nG <3266038380@qq.com>
2026-02-04 17:19:44 +0800

    del

--- AVDownloader/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_AVDownloader_FILE C:/CODE/QTS/Projects/AVDownloader/AVDownloaderWithQTCpp/AVDownloader/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/AVDownloader.exe)
-set(__QT_DEPLOY_TARGET_AVDownloader_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_AVDownloader_RUNTIME_DLLS C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Widgets.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/D/software/QT/6.10.1/mingw_64/bin/Qt6Core.dll)
